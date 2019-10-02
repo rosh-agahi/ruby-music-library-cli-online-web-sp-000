cd222a8f13940cc662818122aba6688e12a4d975
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-02 18:12:10 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,7 +1,16 @@
 class MusicImporter
-  
-  def initialize(file)
-    
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
   end
-  
+
+  def files
+    Dir.entries(path).delete_if { |file| file == "." || file == ".." }
+  end
+
+  def import
+    files.each {|filename| Song.create_from_filename(filename)}
+  end
+
 end
